624479792fee5e4107f17ae6ef859179dacc7077
Bintang Saputra <ahmadbintang.saputra0@gmail.com>
2025-12-23 20:33:12 +0700

    Add Periksa Pasien feature with CRUD operations and UI updates

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -102,6 +102,14 @@
                             </p>
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a href="{{ route('periksa-pasien.index') }}" class="nav-link {{ request()->routeIs('periksa-pasien.*') ? 'active' : '' }}">
+                            <i class="nav-icon fas fa-stethoscope"></i>
+                            <p>
+                                Periksa Pasien
+                            </p>
+                        </a>
+                    </li>
 
                 @endif
 
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\PasienController;
 use App\Http\Controllers\ObatController;
 use App\Http\Controllers\Dokter\JadwalPeriksaController as DokterJadwalPeriksaController;
 use App\Http\Controllers\Pasien\PoliController as PasienPoliController ;
-
+use App\Http\Controllers\Dokter\PeriksaPasienController as DokterPeriksaPasienController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -36,6 +36,9 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
         return view('dokter.dashboard');
     })->name('dokter.dashboard');
     route::resource('jadwal-periksa', DokterJadwalPeriksaController::class);
+    Route::get('periksa-pasien', [DokterPeriksaPasienController::class, 'index'])->name('periksa-pasien.index');
+    Route::get('periksa-pasien/{id}/create', [DokterPeriksaPasienController::class, 'create'])->name('periksa-pasien.create');
+    Route::post('periksa-pasien', [DokterPeriksaPasienController::class, 'store'])->name('periksa-pasien.store');
 });
 
 // Pasien Routes
